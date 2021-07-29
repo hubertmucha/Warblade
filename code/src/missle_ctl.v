12ca68adff21737e955d56cb83e52e4d8a709be4
Natalia Pluta <pluta.natalia.m@gmail.com>
2021-07-29 11:54:20 +0200

    ERROR: double declaration of xpos_in fix

--- code/src/missle_ctl.v
+++ code/src/missle_ctl.v
@@ -15,8 +15,7 @@ module missle_ctl (
     input wire rst,
     input wire [11:0] xpos_in,
     input wire missle_button,
-    input wire [11:0] xpos_in,
-
+    
     output reg [11:0] ypos_out,
     output reg [11:0] xpos_out,
     output reg on_out
